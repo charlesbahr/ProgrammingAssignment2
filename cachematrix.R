17f4da24cc6824cb8b746f8aad9e96d27d6bc70b
unknown <cbahr@USMUYN0H332128.na02.lucent.com>
2015-09-22 23:45:19 -0400

    class project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list of functions that store a matrix
+## The calling function is then able to set the matrix and get its values
+##calling "set" nullifies the "inverse calculated" flag
 
-## Write a short comment describing this function
+
+## call this function to initialize the code and the data
+## example:  b <- makeCacheMatrix(matrix(c(1,2,3,5),2,2))
+##
+##c.bahr code based on the class example
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+  x_inverse <- NULL
+  set <- function(y) {
+    
+    ## asssign to "global" enviroment
+    
+    x<<- y
+    x_inverse <<- NULL
+  }
+  
+  ## return set value
+  
+  get <- function() x
+  
+  setinverse <- function(x_inv) x_inverse <<- x_inv
+  
+  getinverse <- function() x_inverse
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+    
+  }
+  
 
 
-## Write a short comment describing this function
+## cachesolve calculates the matrix inverse if it discovers
+## that the matrix has been changed (and initially)
+## call this code to get the answer
+## example:  cacheSolve(b)
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  xinverse <- x$getinverse()
+  if(!is.null(xinverse)) {
+    message("getting cached inverse")
+    return (xinverse)
+  }
+  datamatrix <- x$get()
+  xinverse <- solve(datamatrix)
+  x$setinverse(xinverse)
+  xinverse
 }
